bef863efd1444e83da0f66675dc34f1e7b1d0d2d
datobser <159797954+datobser@users.noreply.github.com>
2024-03-04 11:31:01 +0100

    Update style-panel.js

--- style-panel.js
+++ style-panel.js
@@ -32,7 +32,7 @@
         }
 
         _submit(e) {
-	    console.log('submit() called')
+	    console.log('submit() called');
             e.preventDefault();
             this.dispatchEvent(new CustomEvent("propertiesChanged", {
                 detail: {
